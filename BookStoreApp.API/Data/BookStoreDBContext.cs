e000fec6d908758bac1acdcca8b0d7600cbd3c31
Alper Karabo Tlou <alper.tlou@gmail.com>
2023-08-29 12:57:09 +0200

    seeding default users

--- BookStoreApp.API/Data/BookStoreDBContext.cs
+++ BookStoreApp.API/Data/BookStoreDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApp.API.Data
@@ -21,8 +22,8 @@ namespace BookStoreApp.API.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BookStoreDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BookStoreDB;Trusted_Connection=True;MultipleActiveResultSets=True");
             }
         }
 
@@ -61,6 +62,60 @@ namespace BookStoreApp.API.Data
                     .HasForeignKey(d => d.AuthorId)
                     .HasConstraintName("FK_Books_ToTable");
             });
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "5d08bb0e-e4b8-4dff-8823-91a1a6a7140c",
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+                new IdentityRole
+                {
+                    Id = "6ddd2d89-446b-415c-9f4d-bc66f05fb050",
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                });
+
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = "44e14083-ebde-4a91-b7e3-c874ba73239c",
+                    Email = "admin@bookstore.com",
+                    NormalizedEmail = "ADMIN@BOOKSTORE.COM",
+                    UserName = "admin@bookstore.com",
+                    NormalizedUserName = "ADMIN@BOOKSTORE.COM",
+                    FirstName = "System",
+                    LastName = "Admin",
+                    PasswordHash = hasher.HashPassword(null, "P@s5w0rD")
+                },
+                new ApplicationUser
+                {
+                    Id = "838144ee-b2f8-4101-87d5-3e081317c0fe",
+                    Email = "user@bookstore.com",
+                    NormalizedEmail = "USER@BOOKSTORE.COM",
+                    UserName = "user@bookstore.com",
+                    NormalizedUserName = "USER@BOOKSTORE.COM",
+                    FirstName = "System",
+                    LastName = "User",
+                    PasswordHash = hasher.HashPassword(null, "P@s5w0rD")
+                });
+
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "6ddd2d89-446b-415c-9f4d-bc66f05fb050",
+                    UserId = "44e14083-ebde-4a91-b7e3-c874ba73239c"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "5d08bb0e-e4b8-4dff-8823-91a1a6a7140c",
+                    UserId = "838144ee-b2f8-4101-87d5-3e081317c0fe"
+                });
 
             OnModelCreatingPartial(modelBuilder);
         }
